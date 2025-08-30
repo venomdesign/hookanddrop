55f9b727e652f6d636b72e57ccf09dc876882847
venomdesign <chris.schweigart@venomdesign.com>
2025-08-29 20:58:31 -0400

    Initial pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,23 @@
+// import { defineConfig } from 'astro/config';
+
+// export default defineConfig({
+//   base: '/hookanddrop/',
+//   site: 'https://hookanddrop.com',
+//   output: 'static',
+//   integrations: [github()]
+// });
+// import github from '@astrojs/github';
+// @ts-check
 import { defineConfig } from 'astro/config';
 
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://hookanddrop.com',
-  output: 'static'
+    base: '/hookanddrop/', // 👈 required for GitHub Pages
+    vite: {
+        resolve: {
+            alias: {
+                '@': new URL('./src', import.meta.url).pathname,
+            },
+        },
+    },
 });
